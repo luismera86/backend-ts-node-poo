1ff337cfb588f4fbb3108acfde3686fe4c23281d
Luis Mera <luismera86@gmail.com>
2023-01-11 17:16:21 -0300

    backend con poo

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import cors from 'cors'
+import express from 'express'
+import morgan from 'morgan'
+
+class ServerBootstrap {
+  public app: express.Application = express()
+  private port: number = 8000
+
+  constructor() {
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
+    this.app.use(morgan('dev'))
+    this.app.use(cors())
+    this.listen()
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Connected on server in port ${this.port}`)
+    })
+  }
+}
+
+new ServerBootstrap()
